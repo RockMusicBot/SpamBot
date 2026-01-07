2722fc9f995b8b8ae98dc546011b6ed1f7544f0d
RockMusicBot <githubmusicbot@gmail.com>
2026-01-07 06:52:59 +0530

    Update dev.py

--- KapilYadav/modules/dev.py
+++ KapilYadav/modules/dev.py
@@ -19,11 +19,11 @@ async def aexec(code, event):
     )
     return await locals()["__aexec"](event)
 
-# --- Registering Handlers for Telethon ---
+# --- Registering Handlers ---
 
 for client in clients:
-    # Eval Command
-    @client.on(events.NewMessage(pattern=fr"{hl}eval(?:\s+(.+))?", incoming=True))
+    # Eval Command: Matches <prefix>eval
+    @client.on(events.NewMessage(pattern=fr"^[./?{hl}]eval(?:\s+(.+))?", incoming=True))
     async def eval_handler(event):
         if event.sender_id != OWNER_ID:
             return
@@ -37,34 +37,39 @@ for client in clients:
         old_stdout = sys.stdout
         redirected_output = sys.stdout = StringIO()
         redirected_error = sys.stderr = StringIO()
-        stdout, stderr, exc = None, None, None
         
         try:
             await aexec(cmd, event)
         except Exception:
-            exc = traceback.format_exc()
+            sys.stderr.write(traceback.format_exc())
         
         stdout = redirected_output.getvalue()
         stderr = redirected_error.getvalue()
         sys.stdout = old_stdout
         sys.stderr = old_stderr
         
-        evaluation = exc or stderr or stdout or "Success"
+        evaluation = ""
+        if stderr:
+            evaluation += stderr
+        elif stdout:
+            evaluation += stdout
+        else:
+            evaluation += "Success"
+
         end = datetime.now()
         ms = (end - start).microseconds / 1000
-
         final_output = f"<b>⥤ ʀᴇsᴜʟᴛ :</b>\n<pre>{evaluation}</pre>\n<b>Time:</b> <code>{ms}ms</code>"
         
         if len(final_output) > 4096:
             with open("output.txt", "w+", encoding="utf8") as f:
                 f.write(str(evaluation))
-            await event.client.send_file(event.chat_id, "output.txt", caption=f"<b>Eval Output</b>", reply_to=event.id, parse_mode="html")
+            await event.client.send_file(event.chat_id, "output.txt", caption="Eval Output", reply_to=event.id)
             os.remove("output.txt")
         else:
             await event.reply(final_output, parse_mode="html")
 
-    # Shell Command
-    @client.on(events.NewMessage(pattern=fr"{hl}sh (?:\s+(.+))?", incoming=True))
+    # Shell Command: Matches /sh or .sh or <hl>sh
+    @client.on(events.NewMessage(pattern=fr"^[./?{hl}]sh(?:\s+(.+))?", incoming=True))
     async def shell_handler(event):
         if event.sender_id != OWNER_ID:
             return
@@ -72,6 +77,9 @@ for client in clients:
         cmd = event.pattern_match.group(1)
         if not cmd:
             return await event.reply("<b>ᴇxᴀᴍᴩʟᴇ :</b> <code>.sh git pull</code>", parse_mode="html")
+
+        # Notify the user that the command is running
+        msg = await event.reply("<code>Running shell command...</code>", parse_mode="html")
 
         try:
             process = subprocess.Popen(
@@ -83,17 +91,18 @@ for client in clients:
             )
             stdout, stderr = process.communicate()
         except Exception as err:
-            return await event.reply(f"<b>ERROR :</b>\n<pre>{err}</pre>", parse_mode="html")
+            return await msg.edit(f"<b>ERROR :</b>\n<pre>{err}</pre>", parse_mode="html")
         
         output = stdout or stderr
-        if not output:
-            output = "None"
+        if not output or output.strip() == "":
+            output = "Command executed with no output."
 
         if len(output) > 4096:
             with open("shell.txt", "w+") as f:
                 f.write(output)
-            await event.client.send_file(event.chat_id, "shell.txt", caption="<b>Shell Output</b>", reply_to=event.id, parse_mode="html")
+            await event.client.send_file(event.chat_id, "shell.txt", caption=f"<code>{cmd}</code>", reply_to=event.id)
+            await msg.delete()
             os.remove("shell.txt")
         else:
-            await event.reply(f"<b>OUTPUT :</b>\n<pre>{output}</pre>", parse_mode="html")
-          
+            await msg.edit(f"<b>OUTPUT :</b>\n<pre>{output}</pre>", parse_mode="html")
+            
